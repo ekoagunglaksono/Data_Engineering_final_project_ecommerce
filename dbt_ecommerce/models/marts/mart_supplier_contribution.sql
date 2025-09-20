eb636a18442aaf1546ecebab99f7eb617490af92
Eko Agung Laksono <ekoagung318@gmail.com>
2025-09-20 19:01:44 +0700

    refactor(marts): update mart_supplier_contribution with supplier_name

--- dbt_ecommerce/models/marts/mart_supplier_contribution.sql
+++ dbt_ecommerce/models/marts/mart_supplier_contribution.sql
@@ -2,11 +2,12 @@
 
 select
     s.supplier_id,
+    s.supplier_name,
     count(distinct p.product_id) as total_products,
     round(avg(p.price), 2) as avg_price
 from {{ ref('dim_suppliers') }} s
 join {{ ref('dim_products') }} p
   on s.supplier_id = p.supplier_id
-group by s.supplier_id
+group by s.supplier_id, s.supplier_name
 order by total_products desc
 limit 20
